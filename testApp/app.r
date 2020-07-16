b1ae0bf12958a83638caea8c34c1836204dae8be
naheim <naheim@gmail.com>
2020-07-16 16:02:48 -0400

    starting initial test app

--- testApp/app.r
+++ testApp/app.r
@@ -1,52 +1,58 @@
 library(shiny)
-
-
 ui <- bootstrapPage(
+	fileInput(inputId="liveFormas", label="Upload a file"),
+	tableOutput("livefile")
 
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
+	selectInput(inputId = "n_breaks",
+			  label = "Number of bins in histogram (approximate):",
+			  choices = c(10, 20, 35, 50),
+			  selected = 20),
 
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
+	checkboxInput(inputId = "individual_obs",
+				label = strong("Show individual observations"),
+				value = FALSE),
 
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
+	checkboxInput(inputId = "density",
+				label = strong("Show density estimate"),
+				value = FALSE),
 
-  plotOutput(outputId = "main_plot", height = "300px"),
+	plotOutput(outputId = "main_plot", height = "300px"),
 
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-    sliderInput(inputId = "bw_adjust",
-                label = "Bandwidth adjustment:",
-                min = 0.2, max = 2, value = 1, step = 0.2)
-  )
+	tableOutput(outputId = "live_counts")
+
+	# Display this only if the density is shown
+	conditionalPanel(condition = "input.density == true",
+	sliderInput(inputId = "bw_adjust",
+				label = "Bandwidth adjustment:",
+				min = 0.2, max = 2, value = 1, step = 0.2)
+	)
+
 
 )
 
-
 server <- function(input, output) {
-  output$main_plot <- reactivePlot(width = 400, height = 300, function() {
+	
+	output$livefile <- renderTable(input$liveFormas)  
+	
+	output$main_plot <- reactivePlot(width = 400, height = 300, function() {
 
-    hist(faithful$eruptions,
-      probability = TRUE,
-      breaks = as.numeric(input$n_breaks),
-      xlab = "Duration (minutes)",
-      main = "Geyser eruption duration")
+		hist(faithful$eruptions,
+		  probability = TRUE,
+		  breaks = as.numeric(input$n_breaks),
+		  xlab = "Duration (minutes)",
+		  main = "Geyser eruption duration")
 
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
-    }
+		if (input$individual_obs) {
+		  rug(faithful$eruptions)
+		}
 
-    if (input$density) {
-      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
-      lines(dens, col = "blue")
-    }
+		if (input$density) {
+		  dens <- density(faithful$eruptions, adjust = input$bw_adjust)
+		  lines(dens, col = "blue")
+		}
 
-  })
+	}
+	)
 
 }
 
