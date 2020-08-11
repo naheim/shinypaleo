b955eeba110439acd3c714ad41c72408a0247ec3
naheim <naheim@gmail.com>
2020-08-11 16:03:14 -0400

    starting new app for live-dead data

---
+++ liveDead/README.md
@@ -0,0 +1,13 @@
+# Test App
+This is a test app.
+
+To run the app in R, you need to have the *shiny* R library installed and run the following code:
+
+````r
+library(shiny)
+runGitHub("shinypaleo", "naheim", subdir="testApp")
+````
+
+Running this code will open a new window/tab in your default browser and load the shiny app. 
+
+When you're done using the app, return to the R console and hit 'esc'.
---
+++ liveDead/app.r
@@ -0,0 +1,63 @@
+library(shiny)
+ui <- bootstrapPage(
+	
+	selectInput(inputId = "n_breaks",
+			  label = "Number of bins in histogram (approximate):",
+			  choices = c(10, 20, 35, 50),
+			  selected = 20),
+
+	checkboxInput(inputId = "individual_obs",
+				label = strong("Show individual observations"),
+				value = FALSE),
+
+	checkboxInput(inputId = "density",
+				label = strong("Show density estimate"),
+				value = FALSE),
+
+	plotOutput(outputId = "main_plot", height = "300px"),
+
+
+	tableOutput(outputId = "livefile"),
+
+	# Display this only if the density is shown
+	conditionalPanel(condition = "input.density == true",
+	sliderInput(inputId = "bw_adjust",
+				label = "Bandwidth adjustment:",
+				min = 0.2, max = 2, value = 1, step = 0.2)
+	)
+
+
+)
+
+server <- function(input, output) {
+	
+	liveforams <- read.delim(file="MilkenEtAl2015_live.txt")
+	colnames(liveforams) <- liveforams[1,]
+	liveforams <- [-1,]
+	liveCounts <- colSums(liveforams[,-(1:3)])
+	
+	output$livefile <- renderTable(t(liveCounts) rownames=TRUE)  
+	
+	output$main_plot <- reactivePlot(width = 400, height = 300, function() {
+
+		hist(faithful$eruptions,
+		  probability = TRUE,
+		  breaks = as.numeric(input$n_breaks),
+		  xlab = "Duration (minutes)",
+		  main = "Geyser eruption duration BOOM!")
+
+		if (input$individual_obs) {
+		  rug(faithful$eruptions)
+		}
+
+		if (input$density) {
+		  dens <- density(faithful$eruptions, adjust = input$bw_adjust)
+		  lines(dens, col = "blue")
+		}
+
+	}
+	)
+
+}
+
+shinyApp(ui = ui, server = server)
