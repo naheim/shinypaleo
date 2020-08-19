c9bebfc3218677dd23556a56b67ea38b14ecdb37
naheim <naheim@gmail.com>
2020-08-19 18:57:39 -0400

    making progress

---
+++ liveDead/R/utils.r
@@ -0,0 +1,65 @@
+# drop empty species and sites
+dropEmpty <- function(live, dead) {
+	# drop empty species
+	temp1 <- colSums(live)
+	temp2 <- colSums(dead)
+	live <- live[, temp1 > 0 | temp2 > 0]
+	dead <- dead[, temp1 > 0 | temp2 > 0]
+	
+	# drop empty sites
+	temp1 <- rowSums(live)
+	temp2 <- rowSums(dead)
+	live <- live[temp1 > 0 | temp2 > 0, ]
+	dead <- dead[temp1 > 0 | temp2 > 0, ]
+	
+	return(list('live'=live, 'dead'=dead))
+}
+
+# Parse Data		
+parseData <- function(x, taxon, env, species, environments) {
+	if(env == "subtidal eel grass") {
+		myEnv <- "sub_eelgrass"
+	} else if(env == "intertidal sand flat") {
+		myEnv <- "inter_barren"
+	}
+	
+	# select taxa
+	if(taxon != "all") {
+		xReduced <- x[,is.element(colnames(x), species$colName[species$Class == taxon])]
+	} else {
+		xReduced <- x
+	}
+	
+	# select environment
+	if(env != "all") {
+		xReduced <- xReduced[environments[2,] == myEnv,]
+	}
+	
+	return(xReduced)
+}
+
+simCalc <- function(live, dead) {
+	if(class(live) == "numeric") {
+		n <- 1
+		sim <- data.frame(matrix(NA, nrow=n, ncol=2, dimnames=list("1", c("pctSim","jaccard"))))
+	} else {
+		n <- nrow(live)
+		sim <- data.frame(matrix(NA, nrow=n, ncol=2, dimnames=list(rownames(live), c("pctSim","jaccard"))))
+	}
+	for(i in 1:n) {
+		if(class(live) == "numeric") {
+			x <- live 
+			y <- dead
+		} else {
+			x <- live[i,] 
+			y <- dead[i,]
+		}
+		comm <- rbind(x[x > 0 | y > 0], y[x > 0 | y > 0])
+		sim$pctSim[i] <- 2*sum(apply(comm, 2, min)) / (sum(comm[1,]) + sum(comm[2,]))
+		nCommon <- ncol(data.frame(comm[,comm[1,] > 0 & comm[2,] > 0])) # common
+		nTotal <- ncol(comm) # all present
+		sim$jaccard[i] <- nCommon / nTotal
+		print(sim)
+	}
+	return(sim)
+}
--- liveDead/app.r
+++ liveDead/app.r
@@ -1,5 +1,6 @@
 library(shiny)
-#library(DT)
+source("R/utils.r")
+
 ui <- fluidPage(
 
 	titlePanel("Live-Dead Analysis, Mugu Lagoon, California"),
@@ -30,16 +31,34 @@ ui <- fluidPage(
 
 		mainPanel(
 			## Number of Sites, species and occurrences (live and dead)
-			h3("Counts of Live and Dead Individuals"),		
+			h3("Counts of Live and Dead Individuals & Species"),		
 			fluidRow(
 				tableOutput(outputId = "env_stats")
 			), 
+			
+			## Live - Dead cross plots
+			fluidRow(
+				h3("Comparisons of Live and Death Assemblages"),
+				plotOutput(outputId = "liveDeadPlots", height = "500px", width = "1000px")
+			),
+			
+			## aggregate Live - Dead similarity
+			fluidRow(
+				h3("Similarity between live and death pooled assembalges"),
+				tableOutput(outputId = "liveDeadSimPooled")
+			),
+			
+			## Live - Dead similarity
+			fluidRow(
+				h3("Similarity between live and death assembalges"),
+				plotOutput(outputId = "liveDeadSim", height = "500px", width = "1000px")
+			),
 			
 			## Locality Map
 			fluidRow(
 				h3("Map of Mugu Lagoon with Sample Locations"),
 				strong("Click on Map to download a larger version."),
-				a(img(src='Warme1971_Map2.png', height = 805, width = 1000), href="https://github.com/naheim/shinypaleo/blob/master/liveDead/www/Warme1971_Map2.png?raw=true"),
+				a(img(src='Warme1971_Map2.png', height = "805px", width = "1000px"), href="https://github.com/naheim/shinypaleo/blob/master/liveDead/www/Warme1971_Map2.png?raw=true"),
 			),
 			
 			## Data table
@@ -50,57 +69,107 @@ ui <- fluidPage(
 	)
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+	
 	species <- read.delim(file="warmeSpecies.tsv")
 	species <- subset(species, Phylum == 'Mollusca') # include only mollusca
 	environments <- read.delim(file="warmeHeader.tsv")
 	
-	liveCounts <- read.delim(file="warmeLive.tsv")
+	liveIn <- read.delim(file="warmeLive.tsv")
 	# drop non-molluscan taxa and those not identified to species
-	liveCounts <- liveCounts[,is.element(colnames(liveCounts), species$colName) & !grepl("_sp", colnames(liveCounts))]
+	liveIn <- liveIn[,is.element(colnames(liveIn), species$colName) & !grepl("_sp", colnames(liveIn))]
+	# drop minor environments
+	liveIn <- liveIn[is.element(environments[2,], c("inter_barren","sub_eelgrass")),]
 	
-	deadCounts <- read.delim(file="warmeDead.tsv")
+	deadIn <- read.delim(file="warmeDead.tsv")
 	# drop non-molluscan taxa and those not identified to species
-	deadCounts <- deadCounts[,is.element(colnames(deadCounts), species$colName) & !grepl("_sp", colnames(deadCounts))]
+	deadIn <- deadIn[,is.element(colnames(deadIn), species$colName) & !grepl("_sp", colnames(deadIn))]
+	# drop minor environments
+	deadIn <- deadIn[is.element(environments[2,], c("inter_barren","sub_eelgrass")),]
+	environments <- environments[,is.element(environments[2,], c("inter_barren","sub_eelgrass"))]
 	
-	# Get env stats		
+	tempCounts <- dropEmpty(liveIn, deadIn)
+	liveCounts <- tempCounts$live
+	deadCounts <- tempCounts$dead
+	
+	# Parse Data		
+	tempLive <- reactive({
+		req(input$taxa, input$enviro)
+		parseData(liveCounts, input$taxa, input$enviro, species, environments)
+	})
+	tempDead <- reactive({
+		req(input$taxa, input$enviro)
+		parseData(deadCounts, input$taxa, input$enviro, species, environments)
+	})
+	
+
+	# Get stats		
 	output$env_stats <- renderTable({
-		# select taxa
-		if(input$taxa == "Bivalvia") {
-			tempLive <- liveCounts[,is.element(colnames(liveCounts), species$colName[species$Class == 'Bivalvia'])]
-			tempDead <- deadCounts[,is.element(colnames(deadCounts), species$colName[species$Class == 'Bivalvia'])]
-		} else if (input$taxa == "Gastropoda") {
-			tempLive <- liveCounts[,is.element(colnames(liveCounts), species$colName[species$Class == 'Gastropoda'])]
-			tempDead <- deadCounts[,is.element(colnames(deadCounts), species$colName[species$Class == 'Gastropoda'])]
-		} else {
-			tempLive <- liveCounts
-			tempDead <- deadCounts
-		}
+		nLiveSite <- rowSums(tempLive())
+		nDeadSite <- rowSums(tempDead())
 		
-		# select environment
-		if(input$enviro == "intertidal sand flat") {
-			tempLive <- tempLive[environments[2,] == "inter_barren",]
-			tempDead <- tempDead[environments[2,] == "inter_barren",]
-		} else if (input$enviro == "subtidal eel grass") {
-			tempLive <- tempLive[environments[2,] == "sub_eelgrass",]
-			tempDead <- tempDead[environments[2,] == "sub_eelgrass",]
-		}
-		
-		nLiveSite <- rowSums(tempLive)
-		nDeadSite <- rowSums(tempDead)
-		
-		nLiveSp <- colSums(tempLive)
-		nDeadSp <- colSums(tempDead)
+		nLiveSp <- colSums(tempLive())
+		nDeadSp <- colSums(tempDead())
 		
 		nSites <- c('live'=length(nLiveSite[nLiveSite > 0]), 'dead'=length(nDeadSite[nDeadSite > 0]))
 		nSpecies <- c('live'=length(nLiveSp[nLiveSp > 0]), 'dead'=length(nDeadSp[nDeadSp > 0]))
-		nOccur <- c('live'=sum(tempLive), 'dead'=sum(tempDead))
+		nOccur <- c('live'=sum(tempLive()), 'dead'=sum(tempDead()))
+		
 		statTable <- rbind("Number of Sites"=nSites, "Number of Speices"=nSpecies, "Number of Occurrences"=nOccur)
 
 	}, rownames=TRUE)
+	
+	# plot live vs. dead
+	output$liveDeadPlots <- renderPlot({
+		nLiveSite2 <- rowSums(tempLive())
+		nDeadSite2 <- rowSums(tempDead()) 
+		nLiveSite <- nLiveSite2[nLiveSite2 > 0 & nDeadSite2 > 0] 
+		nDeadSite <- nDeadSite2[nLiveSite2 > 0 & nDeadSite2 > 0] 
+		
+		nLiveSp2 <- colSums(tempLive())
+		nDeadSp2 <- colSums(tempDead())
+		nLiveSp <- nLiveSp2[nLiveSp2 > 0 & nDeadSp2 > 0] 
+		nDeadSp <- nDeadSp2[nLiveSp2 > 0 & nDeadSp2 > 0] 
+		
+		par(pch=16, mfrow=c(1,2), cex=1.5, las=1)
+		plot(nLiveSp, nDeadSp, xlab="Number of live species", ylab="Number of dead species", main="Species", log="xy")
+		abline(a=0, b=1, lty=2)
+		plot(nLiveSite, nDeadSite, xlab="Number of live specimens", ylab="Number of dead specimens", main="Specimens", log="xy")
+		abline(a=0, b=1, lty=2)
+	})
+	
+	# pooled similarity
+	output$liveDeadSimPooled <- renderTable({
+		sim <- simCalc(colSums(tempLive()), colSums(tempDead()))
+		sim2 <- data.frame("Percent Similarity"=sim$pctSim, "Jaccard Similarity"=sim$jaccard)
+		
+	}, rownames=FALSE)
+	
+	# plot similarity
+	output$liveDeadSim <- renderPlot({
+		sim <- simCalc(tempLive(), tempDead())
+		par(pch=16, cex.axis=1.5, cex.lab=1.5, las=1, mfrow=c(1,2))
+		hist(sim$pctSim, breaks=seq(0,1,0.1), xlab="Percent similarity", ylab="Number of sites", main="Similarity-Abundance")
+		box()
+		hist(sim$jaccard, breaks=seq(0,1,0.1), xlab="Jaccard similarity index", ylab="Number of sites", main="Similarity-Species")
+		box()
+		
+	})
 	
 	output$livefile <- renderTable(liveCounts, rownames=TRUE)  
 	
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
