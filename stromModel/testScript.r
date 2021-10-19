fe32d04bd9a3b83d2de26cfaa34dc240f6f68823
naheim <naheim@gmail.com>
2021-10-19 15:25:37 -0400

    working on sedimentation

--- stromModel/testScript.r
+++ stromModel/testScript.r
@@ -3,13 +3,12 @@ library('raster')
 
 # NUMBER OF TOTAL ITERATIONS
 # how long to run the model for
-total.iter <- 150
+total.iter <- 200
 
 # set raster size
 n.columns <- 201
-n.rows <- 251
-
-row.numbers <- n.columns * 1:(n.rows-1) + 1
+n.rows <- 201
+row.numbers <- rev(n.columns * 1:(n.rows-1) + 1) # the first cell in each row--reversed so we count up from the bottom
 
 ### SET MODEL PARAMETERS
 # RANDOM SEED
@@ -33,11 +32,13 @@ sed.incr <- 1
 
 # INTERVAL BETWEEN SEDIMENTATION INCREMENT 
 # number of iterations between deposition events. 
-sed.int <- 5
+sed.int <- 1
 
 # START-UP INTERVAL
 # number of iterations before first depositional event.
 startup <- 10
+
+
 
 # COLOR SWITCH
 # number of iterations before changing color
@@ -45,8 +46,8 @@ col.switch <- 8
 
 ### SET DATA OBJECTS AND PRINTING PREFERENCES
 # plot colors
-#plot.colors <- c("black","darkgray","tan3") # black & gray alternating growth colors, tan sediment
-plot.colors <- c("black","darkgray") # black & gray alternating growth colors, tan sediment
+plot.colors <- c("black","darkgray","tan3") # black & gray alternating growth colors, tan sediment
+#plot.colors <- c("black","darkgray") # black & gray alternating growth colors, tan sediment
 
 # create raster for holding data
 growth <- raster(matrix(0, nrow=n.rows, ncol=n.columns), xmn=0, xmx=n.columns, ymn=0, ymx=n.rows)
@@ -67,6 +68,12 @@ t0 <- Sys.time()
 
 # initialize fill color to black (1)
 fill.color <- 1
+
+# get row numbers on which to make sediment deposit
+sed.iter <- seq(startup, total.iter, sed.int) # the iterations in which sedimentation occurs
+sed.deposits <- row.numbers[seq(sed.int, length(row.numbers), sed.int)]
+sed.count <- 1
+
 for(i in 1:total.iter) {
 	# determine if fill color needs to be switched
 	if(i %% col.switch == 0 && fill.color == 1) {
@@ -76,7 +83,7 @@ for(i in 1:total.iter) {
 	}
 	
 	# get non-empty cells
-	not.empty <- as.data.frame(adjacent(growth, growth.index[getValues(growth) > 0], sorted=TRUE))
+	not.empty <- as.data.frame(adjacent(growth, growth.index[getValues(growth) > 0 & getValues(growth) < 3], sorted=TRUE))
 	not.empty$value <- growth[not.empty[,2]]
 	not.empty$rel.pos <- not.empty$from - not.empty$to
 	not.empty <- subset(not.empty, value == 0)
@@ -97,20 +104,22 @@ for(i in 1:total.iter) {
 		
 		growth[empty.adj$to] <- empty.adj$new.value
 		
-		# sedimentation
-		# SEDIMENTATION INCREMENT 
-		# if deposition is occurring, how much is dumped in a single event. 
-		sed.incr <- 1
-
-		# INTERVAL BETWEEN SEDIMENTATION INCREMENT 
-		# number of iterations between deposition events. 
-		sed.int <- 5
-
-		# START-UP INTERVAL
-		# number of iterations before first depositional event.
-		startup <- 10
-		
 	}
+	
+	# sedimentation
+	# if enough iterations have passed
+	# start at bottom row, lay down sed -- as many rows as requested
+	if(i == sed.iter[sed.count]) {
+		add.sed <- TRUE
+		for(j in 1:n.columns) {
+			if(growth[sed.deposits[sed.count]:(sed.deposits[sed.count]+n.columns-1)][j] == 0 & add.sed==TRUE) {
+				growth[sed.deposits[sed.count]:(sed.deposits[sed.count]+n.columns-1)][j] <- 3
+			} else if(growth[sed.deposits[sed.count]:(sed.deposits[sed.count]+n.columns-1)][j] > 0) {
+				add.sed <- FALSE
+			}
+		}
+	}
+	
 	if(i %% 10 == 0) print(i)
 }
 t1 <- Sys.time()
@@ -122,7 +131,7 @@ growth.plot <- raster::trim(growth.plot, padding = 20)
 
 ### PLOT
 # convert color matrix to raster and plot
-plot(growth.plot, col=plot.colors, legend=FALSE, axes=FALSE)
+plot(growth.plot, col=plot.colors, legend=FALSE, axes=TRUE)
 
 
 
