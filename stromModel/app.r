c03badfc597d6aaa4e63509e7a31acddd4e82661
naheim <naheim@gmail.com>
2021-10-25 13:11:44 -0400

    debugging

--- stromModel/app.r
+++ stromModel/app.r
@@ -90,6 +90,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {	
 	library('raster')
 	library('spatstat.utils')
+	
 	themodel <- reactive({
 		# NUMBER OF TOTAL ITERATIONS
 		# how long to run the model for
@@ -234,8 +235,9 @@ server <- function(input, output, session) {
 		isolate({### PLOT
 			# convert color matrix to raster and plot
 			par(mar=c(0.2,0.2,0.2,0.2))
-			plot(1:10, type="n", axes=F, xlim=c(0,n.columns), ylim=c(0,n.rows), xlab="", ylab="")
-			raster::plot(as.raster(themodel()), col=plot.colors, legend=FALSE, add=TRUE)
+			#plot(1:10, type="n", axes=F, xlim=c(0,n.columns), ylim=c(0,n.rows), xlab="", ylab="")
+			plot(themodel())
+			#raster::plot(themodel(), col=plot.colors, legend=FALSE, add=TRUE)
 		})
 	})
 }
