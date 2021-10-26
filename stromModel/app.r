cd47e9b59106fdbb6f7c78c1471b3a5fe920cfa9
naheim <naheim@gmail.com>
2021-10-26 10:03:33 -0400

    debugging

--- stromModel/app.r
+++ stromModel/app.r
@@ -236,7 +236,7 @@ server <- function(input, output, session) {
 		isolate({### PLOT
 			# convert color matrix to raster and plot
 			par(mar=c(0.2,0.2,0.2,0.2))
-			plot(1:10, type="n", axes=F, xlim=c(0,raster::ncol(themodel())), ylim=c(0,n.rows), xlab="", ylab="")
+			plot(1:10, type="n", axes=FALSE, xlim=c(0,raster::ncol(themodel())), ylim=c(0,n.rows), xlab="", ylab="")
 			raster::plot(themodel(), col=plot.colors, legend=FALSE, add=TRUE)
 		})
 	})
