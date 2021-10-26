9b815b54ae38269e2b0f324d388099cc2e43f9ae
naheim <naheim@gmail.com>
2021-10-26 12:26:08 -0400

    debugging

--- stromModel/app.r
+++ stromModel/app.r
@@ -211,7 +211,7 @@ server <- function(input, output, session) {
 					#growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)][cumsum(growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)]) == 0] <- 3
 					# fill in from the left
 					#growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)][revcumsum(growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)]) == 0] <- 3		
-				#	sed.bed <- sed.bed + 1
+					sed.bed <- sed.bed + 1
 					#print(paste("i=", i, "; j=",j, sep=""))
 					#print(paste("growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)][revcumsum(growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)]) == 0]", growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)][revcumsum(growth[row.numbers[sed.bed]:(row.numbers[sed.bed]+n.columns-1)]) == 0], sep=""))
 										
