cbdd1d6d61c36660fd86368518e744e5a4ee0ca2
naheim <naheim@gmail.com>
2021-11-03 13:04:01 -0400

    got macrostrat app working

--- macrostrat/app.r
+++ macrostrat/app.r
@@ -1,6 +1,4 @@
 library('shiny')
-library('raster')
-library('spatstat.utils')
 
 ui <- fluidPage(
 
@@ -9,7 +7,14 @@ ui <- fluidPage(
 	sidebarLayout(
 		sidebarPanel(					
 			h3("Set Plotting Options"),
-			h5("The model can take more than a minute to load. Please be patient after you hit the 'Run Model' button.", style="color:red"),
+			h5("The data can take about a minuteto load. Please be patient after you hit the 'Go' button.", style="color:red"),
+			br(),
+			
+			# select project
+			selectInput(inputId = "proj",
+			            label = "Project:",
+			            choices = c("N. America & Carribean","New Zealand"),
+			            selected = "N. America & Carribean"),
 			br(),
 			
 			# select environment
@@ -21,12 +26,13 @@ ui <- fluidPage(
 			
 			# select taxa
 			textInput(inputId = "taxa",
-				label = "Enter a higher taxon (leave blank for all): "),
+				label = "Enter a higher taxon (leave blank for all, seperate multiple by commas): "),
 			br(),
 			
-			br(),						
-			submitButton(" Go "),
-#			actionButton("submitParams", "Run Model"),
+			br(),	
+			
+#			submitButton(" Go "),
+			actionButton("submitParams", " Go "),
 			width=3,
 		),
 		
@@ -44,22 +50,40 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {	
 	macrostrat <- reactive({
-		# read in packages
-		pkg.uri <- URLencode(paste0("https://macrostrat.org/api/sections?project_id=1,7&lith_class=sedimentary&environ_class=",input$enviro,"&interval_name=Phanerozoic&format=csv"))
+	  t0 <- Sys.time()
+	  
+	  # get inputs & timescale
+	  if(input$proj == "N. America & Carribean") {
+		  proj.id <- "1,7"
+		  timescale <- read.csv(file=URLencode("https://macrostrat.org/api/defs/intervals?timescale=international ages&format=csv"), header=TRUE)
+		  timescale <- subset(timescale, t_age >= 0.0117)
+	  } else {
+		  proj.id <- "5"
+		  timescale <- read.csv(file=URLencode("https://macrostrat.org/api/defs/intervals?timescale=New Zealand ages&format=csv"), header=TRUE)
+	  }
+	  nBins <- nrow(timescale)
+	  
+	  if(input$enviro == "all") {
+	    env.call <- ""
+	  } else {
+	    env.call <- paste0("&environ_class=",input$enviro)
+	  }
+	  
+	  # read in packages
+	  # note the use of URLencode, which is needed to convert the space in international ages to unicode
+	  pkg.uri <- URLencode(paste0("https://macrostrat.org/api/sections?project_id=",proj.id,"&lith_class=sedimentary",env.call,"&interval_name=Phanerozoic&format=csv"))
 		packages <- read.csv(file=pkg.uri, header=TRUE)
-		
-		pbdb.uri <- URLencode(paste0("https://macrostrat.org/api/fossils?unit_id=",input$enviro,"&format=csv"))
+		sect.ids <- paste(unique(packages$section_id), collapse=",")
+		units.uri <- URLencode(paste0("https://macrostrat.org/api/units?lith_class=sedimentary",env.call,"&section_id=",sect.ids,"&format=csv"))
+		units <- read.csv(file=units.uri, header=TRUE)
+    unit.ids <- paste(unique(units$unit_id), collapse=",")
+		pbdb.uri <- URLencode(paste0("https://macrostrat.org/api/fossils?lith_class=sedimentary",env.call,"&section_id=",sect.ids,"&format=csv"))
 		fossils <- read.csv(file=pbdb.uri, header=TRUE)
 		
-		# read in timescale (international ages)
-		# note the use of URLencode, which is needed to convert the space in international ages to unicode
-		timescale <- read.csv(file=URLencode("https://macrostrat.org/api/defs/intervals?timescale=international ages&format=csv"), header=TRUE)
-		nBins <- nrow(timescale)
-
-		summaryData <- data.frame('interval'=timescale$name, 'ageMid'=apply(cbind(timescale$b_age, timescale$t_age), 1, mean), 'nPkg'=NA, 'nColl'=NA, 'nFossiliferous'=NA, 'nTrunc'=NA, 'nInit'=NA)
+		summaryData <- data.frame('interval'=timescale$name, 'ageBottom'=timescale$b_age, 'ageTop'=timescale$t_age, 'ageMid'=apply(cbind(timescale$b_age, timescale$t_age), 1, mean), 'nPkg'=NA, 'nColl'=NA, 'nFossiliferous'=NA, 'nTrunc'=NA, 'nInit'=NA)
 
 		for(i in 1:nBins) {
-			tempPkg <- subset(packages.marine, b_age > timescale$t_age[i] & t_age < timescale$b_age[i])
+			tempPkg <- subset(packages, b_age > timescale$t_age[i] & t_age < timescale$b_age[i])
 			summaryData$nPkg[i] <- nrow(tempPkg)
 			summaryData$nColl[i] <- sum(tempPkg$pbdb_collections)
 			summaryData$nFossiliferous[i] <- nrow(subset(tempPkg, pbdb_collections > 0))
@@ -71,53 +95,27 @@ server <- function(input, output, session) {
 			summaryData$nInit[i] <- nrow(tempInit)
 	
 		}
-
-		quartz(height=6, width=12)
-		par(mfrow=c(1,3), las=1, pch=16)
-		plot(summaryData$ageMid, summaryData$nPkg, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Number of marine packages")
-		plot(summaryData$ageMid, summaryData$nColl, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Number of PBDB collections")
-		plot(summaryData$ageMid, summaryData$nFossiliferous/summaryData$nPkg, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Proportion of fossilifeous packages")
-
-
-
-		### Now for non-marine rocks -- hint you can copy and paste above code, then just replace the environ_class in the packages call (and delete the timescale, you can use the one from above)
-		### I also gave my summary data frame a new name, just to keep both
-		# read in packages
-		packages.nonmarine <- read.csv(file="https://macrostrat.org/api/sections?project_id=1,7&lith_class=sedimentary&environ_class=non-marine&interval_name=Phanerozoic&format=csv", header=TRUE)
-
-
-		summaryDataTerr <- data.frame('interval'=timescale$name, 'ageMid'=apply(cbind(timescale$b_age, timescale$t_age), 1, mean), 'nPkg'=NA, 'nColl'=NA, 'nFossiliferous'=NA, 'nTrunc'=NA, 'nInit'=NA)
-
-		for(i in 1:nBins) {
-			tempPkg <- subset(packages.nonmarine, b_age > timescale$t_age[i] & t_age < timescale$b_age[i])
-			summaryDataTerr$nPkg[i] <- nrow(tempPkg)
-			summaryDataTerr$nColl[i] <- sum(tempPkg$pbdb_collections)
-			summaryDataTerr$nFossiliferous[i] <- nrow(subset(tempPkg, pbdb_collections > 0))
-	
-			tempTrunc <- subset(tempPkg, t_age >= timescale$t_age[i] & t_age < timescale$b_age[i])
-			summaryDataTerr$nTrunc[i] <- nrow(tempTrunc)
-	
-			tempInit <- subset(tempPkg, b_age > timescale$t_age[i] & b_age <= timescale$b_age[i])
-			summaryDataTerr$nInit[i] <- nrow(tempInit)
-	
-		}
-
-		quartz(height=6, width=12)
-		par(mfrow=c(1,3), las=1, pch=16)
-		plot(summaryDataTerr$ageMid, summaryDataTerr$nPkg, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Number of non-marine packages")
-		plot(summaryDataTerr$ageMid, summaryDataTerr$nColl, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Number of PBDB collections")
-		plot(summaryDataTerr$ageMid, summaryDataTerr$nFossiliferous/summaryDataTerr$nPkg, type="l", xlim=c(541,0), xlab="Geologic time (Ma)", ylab="Proportion of fossilifeous packages")
-
+		t1 <- Sys.time()
+		print(t1 - t0)
 		
+		summaryData
 	})
 	
 	output$timeseries <- renderPlot({
+		sumData <- macrostrat()
+		drop.ints <- rev(cumsum(rev(sumData$nPkg)))
+		sumData <- sumData[drop.ints > 0,]
 		
+		x.lim <- c(max(sumData$ageBottom), min(sumData$ageTop))
+		
+	  input$submitParams
 		isolate({### PLOT
 			# convert color matrix to raster and plot
-			par(mar=c(0.2,0.2,0.2,0.2))
-			plot(1:10, type="n", axes=FALSE, xlim=xLimits, ylim=yLimits, xlab="", ylab="")
-			plot(themodel(), col=plot.colors, legend=FALSE, add=TRUE)
+		  par(mfrow=c(1,3), las=1, pch=16, cex=1.3)
+		  plot(sumData$ageMid, sumData$nPkg, type="l", xlim=x.lim, xlab="Geologic time (Ma)", ylab="Number of marine packages", main="Packages")
+		  plot(sumData$ageMid, sumData$nColl, type="l", xlim=x.lim, xlab="Geologic time (Ma)", ylab="Number of PBDB collections", main="Fossil Collections")
+		  plot(sumData$ageMid, sumData$nFossiliferous/sumData$nPkg, type="l", xlim=x.lim, xlab="Geologic time (Ma)", ylab="Proportion of fossilifeous packages", main="Proportion Fossiliferous")
+		  
 		})
 	})
 }
